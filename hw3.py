2d48e064bb189394d6073d2a44209a73b4592370
Nahid Hossain <mdnhossain11@gmail.com>
2022-01-27 21:35:13 -0500

    final update

--- hw3.py
+++ hw3.py
@@ -1,4 +1,5 @@
 import math
+import csv
 import os
 def letterChecker(string):
     vowelCounter = 0
@@ -21,10 +22,20 @@ def cylinderVolume(h,r):
 def stringListCombiner(stringList):
     stringListCombined=",".join(stringList)
     return stringListCombined
-#def nestedListToCSV():
-
-#def CSVtoNestedList():
-
+def nestedListToCSV():
+    fields = ["First Name", "Last Name"]
+    rows = [["Nahid","Hossain"],["Luke","Walter"],["Anton","Shayakhmetov"],["Brian","Huang"],["Abdul","Rahman"],["Jay","Patel"],["Joshua","Biswas"],["Tim","Wargo"],["Eric","Phung"],["Adam","Davis"],["Taman","Ho"],["Brian","Compau"]]
+    filename = "friends.csv"
+    with open(filename, 'w') as csvfile:
+        csvwriter = csv.writer(csvfile)
+        csvwriter.writerow(fields)
+        csvwriter.writerows(rows)
+    return "friends.csv has been created"
+def CSVtoNestedList():
+    with open("friends.csv", mode='r') as file:
+        csvFile = csv.reader(file)
+        for lines in csvFile:
+            print(lines)
 def divisionWithWarning(m,n):
     try:
         o=m/n
@@ -43,6 +54,9 @@ def createFolder():
 print(letterChecker("Ukulele"))
 print(letterChecker("Banjo"))
 print(cylinderVolume(2,3))
+print(stringListCombiner(["apple","pen","pinepple","pen"]))
+print(nestedListToCSV())
+CSVtoNestedList()
 print(divisionWithWarning(2,1))
 print(divisionWithWarning(2,0))
 print(duplicateRemover([1,1,2,3,4,5,5]))
